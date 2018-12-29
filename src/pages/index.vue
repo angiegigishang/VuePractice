b128ded581d274a0aee9f9ff7f61e17193d3dc92
shangxy <shangxy@zjrealtech.com>
2018-12-29 15:20:51 +0800

    主页左侧列表栏样式完成

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -24,6 +24,21 @@
 				</ul>
 			</div>
 		</div>
+		<div class="index-right">
+			<div class="index-board-list">
+				<div class="index-board-item"
+				     v-for="(item, index) in boardList"
+				     :class="[{'line-last' : index % 2 !== 0}, 'index-board-' + item.id]">
+					<div class="index-board-item-inner">
+						<h2>{{item.title}}</h2>
+						<p>{{item.description}}</p>
+						<div class="index-board-button">
+							<router-link class="button" :to="{path: 'detail/' + item.toKey}">立即购买</router-link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -31,6 +46,36 @@
 export default {
 	data () {
 		return {
+			boardList: [
+			        {
+			          title: '开放产品',
+			          description: '开放产品是一款开放产品',
+			          id: 'car',
+			          toKey: 'analysis',
+			          saleout: false
+			        },
+			        {
+			          title: '品牌营销',
+			          description: '品牌营销帮助你的产品更好地找到定位',
+			          id: 'earth',
+			          toKey: 'count',
+			          saleout: false
+			        },
+			        {
+			          title: '使命必达',
+			          description: '使命必达快速迭代永远保持最前端的速度',
+			          id: 'loud',
+			          toKey: 'forecast',
+			          saleout: true
+			        },
+			        {
+			          title: '勇攀高峰',
+			          description: '帮你勇闯高峰，到达事业的顶峰',
+			          id: 'hill',
+			          toKey: 'publish',
+			          saleout: false
+			        }
+			      ],
 			newsList: [
 		            {
 		              title: '数据统计',
@@ -110,6 +155,9 @@ export default {
 		float: left
 		width: 300px
 		text-align: left
+	.index-right
+		float: left
+		width: 900px
 	.index-left-block
 		margin: 15px
 		background: #fff
@@ -137,4 +185,50 @@ export default {
 	.hot-tag
 		background: red
 		color: white
+	.index-board-item
+		float: left
+		width: 400px
+		background: #fff
+		box-shadow: 0 0 1px #ddd
+		padding: 20px
+		margin-right: 20px
+		margin-bottom: 20px
+	.index-board-item-inner
+		min-height: 125px
+		padding-left: 120px
+	.index-board-car .index-board-item-inner{
+	  background: url(../assets/images/1.png) no-repeat;
+	}
+	.index-board-loud .index-board-item-inner{
+	  background: url(../assets/images/2.png) no-repeat;
+	}
+	.index-board-earth .index-board-item-inner{
+	  background: url(../assets/images/3.png) no-repeat;
+	}
+	.index-board-hill .index-board-item-inner{
+	  background: url(../assets/images/4.png) no-repeat;
+	}
+	.index-board-item h2
+		font-size: 18px
+		font-weight: bold
+		color: #000
+		margin-bottom: 15px
+	.line-last
+		margin-right: 0
+	.index-board-button
+		margin-top: 20px
+	.lastest-news
+		min-height: 512px
+	.new-item
+		display: inline-block
+		width: 230px
+		overflow: hidden
+		text-overflow: ellipsis
+		white-space: nowrap
+	.button
+		background: #4fc08d
+		color: #fff
+		display: inline-block
+		padding: 10px 20px
+		cursor: pointer
 </style>
